42bf8ef8944994cda343d2db2b11bf2ff4fda232
Safarov <elkhan.safarov10@yandex.com>
2020-07-14 11:37:13 +0500

    Создание компоента формы входа

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="login">
+        <h2>Вход</h2>
+        <input type="email" placeholder="Электронная почта">
+        <input type="password" placeholder="Пароль">
+        <button class="btn">Войти</button>
+    </div>
+</template>
+<script>
+export default{
+    created() {
+        console.log('Логин')
+    }
+}
+</script>
