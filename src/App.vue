e51131ffcabde02970972240c97e12c3025a8ee5
Safarov <elkhan.safarov10@yandex.com>
2020-07-14 11:54:20 +0500

    Создание компонента Stories

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
     <hello></hello>
-    <login></login>
+    <login></login> -->
+    <register></register>
   </div>
 </template>
 
 <script>
-import Login from './components/Login.vue'
-import Hello from './components/Hello.vue'
+// import Login from './components/Login.vue'
+// import Hello from './components/Hello.vue'
+import Register from './components/Register'
 
 export default {
   name: 'App',
   components: {
-    Hello,
-    Login
+    // Hello,
+    // Login,
+    Register
   }
 }
 </script>
