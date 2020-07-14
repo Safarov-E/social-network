2e9d7540c0f0ba2435ade4482bca433e443ef702
Safarov <elkhan.safarov10@yandex.com>
2020-07-14 13:34:54 +0500

    Создание компонента Famous

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <hello></hello>
-    <login></login> -->
-    <register></register>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <hello></hello>
+    <login></login>
+    <register></register> -->
+    <stories></stories>
   </div>
 </template>
 
 <script>
 // import Login from './components/Login.vue'
 // import Hello from './components/Hello.vue'
-import Register from './components/Register'
+// import Register from './components/Register'
+import Stories from './components/Stories'
 
 export default {
   name: 'App',
   components: {
     // Hello,
     // Login,
-    Register
+    // Register,
+    Stories
   }
 }
 </script>
