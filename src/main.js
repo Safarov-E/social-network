b7f6ea8a647fa470877a6dce3748e6726104e648
Safarov <elkhan.safarov10@yandex.com>
2020-07-14 19:32:57 +0500

    Vue Router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import Hello from '../src/components/Hello'
+import Login from '../src/components/Login'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Hello },
+  { path: '/login', component: Login }
+]
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+const router = new VueRouter({
+  routes
+})
