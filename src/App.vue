c24ff44a02ea9313199ade529529c2346a81cb53
Safarov <elkhan.safarov10@yandex.com>
2020-07-14 11:45:42 +0500

    Создание компоента формы регистрации

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <Hello msg="Welcome to Your Vue.js App"/>
+    <hello></hello>
+    <login></login>
   </div>
 </template>
 
 <script>
+import Login from './components/Login.vue'
 import Hello from './components/Hello.vue'
 
 export default {
   name: 'App',
   components: {
-    Hello
+    Hello,
+    Login
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Register.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="register">
+        <h2>Форма регистрации</h2>
+        <input type="text" placeholder="Имя" class="form-control">
+        <input type="text" placeholder="Фамилия" class="form-control">
+        <input type="text" placeholder="Электронная почта" class="form-control">
+        <input type="text" placeholder="Пароль" class="form-control">
+        <input type="text" placeholder="Подтверждение пароля" class="form-control">
+        <button type="submit" class="btn">Зарегистрироваться</button>
+    </div>
+</template>
+<script>
+    export default {
+        created() {
+            console.log('регистрация')
+        }
+    }
+</script>
