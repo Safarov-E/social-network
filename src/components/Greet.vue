1ca62e6f05c24172bddb2ddec48ba02bd9d9d999
Safarov <elkhan.safarov10@yandex.com>
2020-07-14 14:36:24 +0500

    Создание компонента Greet

---
+++ src/components/Greet.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="greet">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            msg: 'Нет! Я хочу использовать элемент <component>!'
+        }
+    }
+}
+</script>
